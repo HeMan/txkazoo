ff9ff53cdfb52e4c109d5f715b18d636f9eb4f94
Manish Tomar <manish.tomar@rackspace.com>
2014-01-02 12:05:49 -0800

    version update for log change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='txkazoo',
-    version='0.0.2',
+    version='0.0.3',
     description='Twisted binding for Kazoo',
     maintainer='Manish Tomar',
     maintainer_email='manish.tomar@rackspace.com',
