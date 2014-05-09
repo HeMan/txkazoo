b785a0dd83b6521d62e5d52c5c4c8115fa4b98fc
Laurens Van Houtven <_@lvh.cc>
2014-05-09 18:52:59 -0500

    Add long_description from README + URL

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name='txkazoo',
     version='0.0.4',
+
     description='Twisted binding for Kazoo',
+    long_description=open("README.md").read(),
+    url="https://github.com/rackerlabs/txkazoo",
+
     maintainer='Manish Tomar',
     maintainer_email='manish.tomar@rackspace.com',
     license='Apache 2.0',
+
     packages=find_packages(),
     install_requires=['twisted==13.2.0', 'kazoo==2.0b1']
 )
