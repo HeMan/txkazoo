b101bc38a649c77c113d9a752014d996e3650740
Laurens Van Houtven <_@lvh.cc>
2014-05-12 07:32:21 +0200

    Add a bunch of metadata to setup.py

--- setup.py
+++ setup.py
@@ -17,15 +17,28 @@ dependencies = map(str.split, read("requirements.txt").split())
 setup(
     name=package_name,
     version=version_string,
-
     description='Twisted binding for Kazoo',
     long_description=open("README.md").read(),
     url="https://github.com/rackerlabs/txkazoo",
 
+    author='Manish Tomar',
+    author_email='manish.tomar@rackspace.com',
     maintainer='Manish Tomar',
     maintainer_email='manish.tomar@rackspace.com',
+
     license='Apache 2.0',
+    keywords="crypto urwid twisted",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Distributed Computing"
+    ],
 
     packages=find_packages(),
-    install_requires=dependencies
+    install_requires=dependencies,
+    zip_safe=True
 )
